9d4671c400106c28fdab863ad833053aad4e4d1e
Oliver Infante <92893438+Inf015@users.noreply.github.com>
2023-02-07 20:46:29 -0400

    C#

---
+++ C#/Code/HelloWorld/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           string f1, f2, f3;
+
+        f1 = "Niki";
+        f2 = "Omar";
+        f3 = "Steven";
+
+        GetFriend(f1);
+        GetFriend(f2);
+        GetFriend(f3);
+        
+        }
+        public static void GetFriend(string FriendName)
+        {
+            System.Console.WriteLine("Hi "+ FriendName + " My friend");
+            
+        }
+    }
+}
---
+++ C#/Code/test/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Coding.Exercise
+{
+    public class Method
+    {
+        // Place for your methods
+        public static void LowUpper(string LowUpper)
+        {
+            Console.WriteLine(properString.ToUpper(s));
+            Console.WriteLine(properString.ToLower(s));
+        }
+        
+        public static void Run()
+        {
+            // We encourage you to test your code with different strings,
+            // but don't forget to put the default string back at the end of your testing.
+            string s = "HeY ThErE !";
+            
+            /// Change nothing down here 
+            s = LowUpper(s);
+            Console.WriteLine(s);
+            Count(s);
+            
+        }
+    }
+}
