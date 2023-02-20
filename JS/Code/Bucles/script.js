afbd0072853a36473a64919be019ae97252b23e4
Oliver Infante <92893438+Inf015@users.noreply.github.com>
2023-02-20 17:08:13 -0400

    bucles

---
+++ JS/Code/Bucles/script.js
@@ -0,0 +1,21 @@
+// let num = 0
+
+// if(num<5){
+//     num++
+//     document.write(num)
+// }
+
+// let num2 = 0
+
+// while(num2<5){
+//     num2++
+//     document.write(num2)
+// }
+let num3 = 0
+
+do{
+    document.write(num3)
+    num3++
+
+}
+while(num3<5)
