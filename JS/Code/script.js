06db5a4315ffc51becf463cd3492ec059af33b53
Oliver Infante <92893438+Inf015@users.noreply.github.com>
2023-02-20 16:32:19 -0400

    Associative arrays

--- JS/Code/script.js
+++ JS/Code/script.js
@@ -1,1 +1,12 @@
-prompt("Hola")
+let PC= {
+    Nombre: "OliverPC",
+    Procesador: "Intel I7 77000",
+    Ram: "32Gb",
+    Espacio : "1TB"
+}
+
+let Nombre = PC["Nombre"]
+let Procesador = PC["Procesador"]
+let Ram = PC["Ram"]
+let Espacio = PC["Espacio"]
+alert(Espacio)
