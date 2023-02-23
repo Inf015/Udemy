18c7423e18b223d0ddde57015daef571b51f89d6
Oliver Infante <92893438+Inf015@users.noreply.github.com>
2023-02-23 11:49:23 -0400

    Functions

---
+++ JS/Code/Functions/script.js
@@ -0,0 +1,11 @@
+function saludar(){
+    respuesta = prompt("Hola como estas?")
+    if (respuesta == "bien") {
+        alert("Que bueno")
+        
+    }else{
+        alert("Que mal")
+    }
+}
+saludar()
+saludar()
